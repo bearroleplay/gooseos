2380c465c4faad79c15b901fd3f2aca9929fed96
bearroleplay <veronakoz6@gmail.com>
2026-02-05 15:00:45 +0600

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,55 +4,47 @@
 #include "fs.h"
 #include "libc.h"
 #include "cmos.h"
-#include "graphics.h"  // <-- ДОБАВЬ
+#include "graphics.h"  // ДОБАВЬ ЭТУ СТРОКУ
 
 void kernel_main(uint32_t magic, void* mb_info) {
     (void)magic;
     (void)mb_info;
     
-    // Включаем графический режим!
+    // Переключаемся в графический режим
     vga_set_mode_13h();
     
-    // Устанавливаем стандартную VGA палитру
+    // Устанавливаем палитру
     for (int i = 0; i < 16; i++) {
-        // Простая палитра (можно улучшить)
         vga_set_palette(i, 
-            (i & 1) ? 63 : 0,        // R
-            (i & 2) ? 63 : 0,        // G  
-            (i & 4) ? 63 : 0);       // B
+            (i & 1) ? 42 : 0,
+            (i & 2) ? 42 : 0,
+            (i & 4) ? 42 : 0);
     }
     
-    // Очищаем экран синим
-    clear_screen(COLOR_BLUE);
-    
     // Рисуем тестовую графику
+    clear_screen(COLOR_BLUE);
     fill_rect(50, 50, 100, 80, COLOR_GREEN);
     draw_rect(55, 55, 90, 70, COLOR_RED);
     draw_line(0, 0, 319, 199, COLOR_WHITE);
     draw_line(319, 0, 0, 199, COLOR_WHITE);
     draw_circle(160, 100, 40, COLOR_YELLOW);
     
-    // Текстовая надпись (пока простыми пикселями)
-    for (int i = 0; i < 10; i++) {
-        draw_pixel(150 + i, 180, COLOR_WHITE);
-        draw_pixel(150 + i, 181, COLOR_WHITE);
-    }
-    
-    // Ждём нажатия клавиши
+    // Инициализируем клавиатуру
     keyboard_init();
     
-    // Бесконечный цикл
+    // Ждём нажатия 'q' для возврата в терминал
     while (1) {
         char key = keyboard_getch();
         if (key == 'q') {
-            // Возвращаемся в текстовый режим
-            vga_set_mode_text();
+            vga_set_mode_text();  // Возвращаемся в текстовый режим
             
             // Инициализируем терминал
+            vga_init();
             fs_init();
             terminal_init();
+            terminal_show_prompt();
             
-            // Запускаем обычный терминал
+            // Запускаем терминал
             while (1) {
                 char key = keyboard_getch();
                 if (key) terminal_handle_input(key);
@@ -60,12 +52,5 @@ void kernel_main(uint32_t magic, void* mb_info) {
                 for (volatile int i = 0; i < 5000; i++);
             }
         }
-        
-        // Простая анимация
-        static int x = 0;
-        draw_pixel(x % 320, 190, COLOR_WHITE);
-        x++;
-        
-        for (volatile int i = 0; i < 10000; i++);
     }
 }

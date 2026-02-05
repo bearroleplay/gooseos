b7535756958730175773914f68ef1eeea1f4396c
bearroleplay <veronakoz6@gmail.com>
2026-02-05 15:04:46 +0600

    Update graphics.h

--- kernel/graphics.h
+++ kernel/graphics.h
@@ -3,13 +3,8 @@
 
 #include <stdint.h>
 
-// УБРАТЬ эти строки - они конфликтуют с vga.h
-// extern uint8_t* vga_buffer;
-// extern int screen_width;
-// extern int screen_height;
-
-// Вместо этого объявим свои переменные с другими именами
-extern uint8_t* gfx_buffer;      // Графический буфер
+// Графический буфер
+extern uint8_t* gfx_buffer;
 extern int gfx_width;
 extern int gfx_height;
 
@@ -23,23 +18,25 @@ void draw_line(int x1, int y1, int x2, int y2, uint8_t color);
 void draw_rect(int x, int y, int w, int h, uint8_t color);
 void fill_rect(int x, int y, int w, int h, uint8_t color);
 void draw_circle(int x0, int y0, int radius, uint8_t color);
+void draw_char(int x, int y, char c, uint8_t color);
+void draw_text(int x, int y, const char* text, uint8_t color);
 
-// Цвета VGA
-#define COLOR_BLACK     0
-#define COLOR_BLUE      1
-#define COLOR_GREEN     2      // <-- ДОЛЖЕН БЫТЬ!
-#define COLOR_CYAN      3
-#define COLOR_RED       4      // <-- ДОЛЖЕН БЫТЬ!
-#define COLOR_MAGENTA   5
-#define COLOR_BROWN     6
-#define COLOR_LIGHT_GRAY 7
-#define COLOR_DARK_GRAY 8
-#define COLOR_LIGHT_BLUE 9
-#define COLOR_LIGHT_GREEN 10
-#define COLOR_LIGHT_CYAN 11
-#define COLOR_LIGHT_RED 12
+// Цвета VGA (первые 16 цветов)
+#define COLOR_BLACK         0
+#define COLOR_BLUE          1
+#define COLOR_GREEN         2
+#define COLOR_CYAN          3
+#define COLOR_RED           4
+#define COLOR_MAGENTA       5
+#define COLOR_BROWN         6
+#define COLOR_LIGHT_GRAY    7
+#define COLOR_DARK_GRAY     8
+#define COLOR_LIGHT_BLUE    9
+#define COLOR_LIGHT_GREEN   10
+#define COLOR_LIGHT_CYAN    11
+#define COLOR_LIGHT_RED     12
 #define COLOR_LIGHT_MAGENTA 13
-#define COLOR_YELLOW    14     // <-- ДОЛЖЕН БЫТЬ!
-#define COLOR_WHITE     15
+#define COLOR_YELLOW        14
+#define COLOR_WHITE         15
 
 #endif

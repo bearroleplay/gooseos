e81d8cc05f63a30b2c41243dc513d58911679f06
bearroleplay <veronakoz6@gmail.com>
2026-02-05 15:01:21 +0600

    Update graphics.h

--- kernel/graphics.h
+++ kernel/graphics.h
@@ -27,7 +27,19 @@ void draw_circle(int x0, int y0, int radius, uint8_t color);
 // Цвета VGA
 #define COLOR_BLACK     0
 #define COLOR_BLUE      1
-// ... остальные цвета
+#define COLOR_GREEN     2      // <-- ДОЛЖЕН БЫТЬ!
+#define COLOR_CYAN      3
+#define COLOR_RED       4      // <-- ДОЛЖЕН БЫТЬ!
+#define COLOR_MAGENTA   5
+#define COLOR_BROWN     6
+#define COLOR_LIGHT_GRAY 7
+#define COLOR_DARK_GRAY 8
+#define COLOR_LIGHT_BLUE 9
+#define COLOR_LIGHT_GREEN 10
+#define COLOR_LIGHT_CYAN 11
+#define COLOR_LIGHT_RED 12
+#define COLOR_LIGHT_MAGENTA 13
+#define COLOR_YELLOW    14     // <-- ДОЛЖЕН БЫТЬ!
 #define COLOR_WHITE     15
 
 #endif

d2b68638ff4a0550aa7cdcb73cae995f361f9df8
bearroleplay <veronakoz6@gmail.com>
2026-02-05 15:04:27 +0600

    Update graphics.c

--- kernel/graphics.c
+++ kernel/graphics.c
@@ -3,7 +3,17 @@
 
 // VGA порты
 #define VGA_AC_INDEX 0x3C0
-// ... остальные дефайны
+#define VGA_MISC_WRITE 0x3C2
+#define VGA_SEQ_INDEX 0x3C4
+#define VGA_SEQ_DATA 0x3C5
+#define VGA_DAC_READ_INDEX 0x3C7
+#define VGA_DAC_WRITE_INDEX 0x3C8
+#define VGA_DAC_DATA 0x3C9
+#define VGA_GC_INDEX 0x3CE
+#define VGA_GC_DATA 0x3CF
+#define VGA_CRTC_INDEX 0x3D4
+#define VGA_CRTC_DATA 0x3D5
+#define VGA_INSTAT_READ 0x3DA
 
 // Видеопамять для режима 13h (320x200, 256 цветов)
 uint8_t* gfx_buffer = (uint8_t*)0xA0000;
@@ -19,15 +29,147 @@ void vga_set_mode_13h(void) {
     );
 }
 
-// ... остальные функции ИЗМЕНИТЬ:
-void clear_screen(uint8_t color) {
-    memset(gfx_buffer, color, 320 * 200);  // gfx_buffer вместо vga_buffer
+// Возврат в текстовый режим (80x25)
+void vga_set_mode_text(void) {
+    __asm__ volatile(
+        "mov $0x03, %%ax\n"
+        "int $0x10\n"
+        : : : "ax"
+    );
 }
 
+// Установка палитры VGA
+void vga_set_palette(uint8_t index, uint8_t r, uint8_t g, uint8_t b) {
+    __asm__ volatile(
+        "movb %1, %%al\n"
+        "movw $0x3C8, %%dx\n"
+        "outb %%al, %%dx\n"
+        "movb %2, %%al\n"
+        "inc %%dx\n"
+        "outb %%al, %%dx\n"
+        "movb %3, %%al\n"
+        "outb %%al, %%dx\n"
+        "movb %4, %%al\n"
+        "outb %%al, %%dx\n"
+        : 
+        : "i"((uint8_t)index),
+          "r"((uint8_t)(r >> 2)),  // VGA использует 6 бит на цвет
+          "r"((uint8_t)(g >> 2)),
+          "r"((uint8_t)(b >> 2))
+        : "al", "dx"
+    );
+}
+
+// Заливка экрана цветом
+void clear_screen(uint8_t color) {
+    memset(gfx_buffer, color, 320 * 200);
+}
+
+// Рисование пикселя
 void draw_pixel(int x, int y, uint8_t color) {
     if (x >= 0 && x < gfx_width && y >= 0 && y < gfx_height) {
-        gfx_buffer[y * gfx_width + x] = color;  // gfx_width вместо screen_width
+        gfx_buffer[y * gfx_width + x] = color;
     }
 }
 
-// ... аналогично во всех функциях
+// Рисование линии (алгоритм Брезенхэма)
+void draw_line(int x1, int y1, int x2, int y2, uint8_t color) {
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
+    
+    while(1) {
+        draw_pixel(x1, y1, color);
+        if (x1 == x2 && y1 == y2) break;
+        int e2 = err * 2;
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+// Рисование прямоугольника (контур)
+void draw_rect(int x, int y, int w, int h, uint8_t color) {
+    // Верхняя и нижняя линии
+    for (int i = x; i < x + w; i++) {
+        draw_pixel(i, y, color);
+        draw_pixel(i, y + h - 1, color);
+    }
+    // Левая и правая линии
+    for (int i = y; i < y + h; i++) {
+        draw_pixel(x, i, color);
+        draw_pixel(x + w - 1, i, color);
+    }
+}
+
+// Рисование залитого прямоугольника
+void fill_rect(int x, int y, int w, int h, uint8_t color) {
+    for (int i = y; i < y + h; i++) {
+        for (int j = x; j < x + w; j++) {
+            draw_pixel(j, i, color);
+        }
+    }
+}
+
+// Рисование круга (алгоритм средней точки)
+void draw_circle(int x0, int y0, int radius, uint8_t color) {
+    int x = radius;
+    int y = 0;
+    int err = 0;
+    
+    while (x >= y) {
+        draw_pixel(x0 + x, y0 + y, color);
+        draw_pixel(x0 + y, y0 + x, color);
+        draw_pixel(x0 - y, y0 + x, color);
+        draw_pixel(x0 - x, y0 + y, color);
+        draw_pixel(x0 - x, y0 - y, color);
+        draw_pixel(x0 - y, y0 - x, color);
+        draw_pixel(x0 + y, y0 - x, color);
+        draw_pixel(x0 + x, y0 - y, color);
+        
+        y += 1;
+        err += 1 + 2 * y;
+        if (2 * (err - x) + 1 > 0) {
+            x -= 1;
+            err += 1 - 2 * x;
+        }
+    }
+}
+
+// Простая функция для вывода текста (8x8 шрифт)
+void draw_char(int x, int y, char c, uint8_t color) {
+    // Простая реализация - заглушка
+    // Позже можно добавить bitmap шрифт
+    if (c >= 'A' && c <= 'Z') c = c - 'A' + 'a';
+    
+    switch(c) {
+        case 'a': case 'A':
+            fill_rect(x+2, y, 4, 7, color);
+            draw_pixel(x+1, y+1, color);
+            draw_pixel(x+6, y+1, color);
+            draw_pixel(x, y+4, color);
+            draw_pixel(x+7, y+4, color);
+            break;
+        // ... другие символы
+        default:
+            // Прямоугольник для неизвестных символов
+            draw_rect(x, y, 8, 8, color);
+            break;
+    }
+}
+
+void draw_text(int x, int y, const char* text, uint8_t color) {
+    int pos = 0;
+    while (*text) {
+        draw_char(x + pos * 9, y, *text, color);
+        text++;
+        pos++;
+    }
+}

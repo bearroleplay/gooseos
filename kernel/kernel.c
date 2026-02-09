f373b4f14855a51e1c5e6148ccfc4ec1433d2bd3
gooseos <veronakoz6@gmail.com>
2026-02-09 15:19:31 +0600

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,23 +4,26 @@
 #include "fs.h"
 #include "libc.h"
 #include "cmos.h"
-#include "graphics.h"  // <-- ДОБАВЬ
+#include "graphics.h"
 
 void kernel_main(uint32_t magic, void* mb_info) {
     (void)magic;
     (void)mb_info;
-  keyboard_init();
-    fs_init(); // <-- ВАЖНО: Инициализируем файловую систему ПЕРВОЙ!
-    terminal_init(); // <-- Теперь терминал знает про FS
-
+    
+    keyboard_init();
+    fs_init();          // Инициализация файловой системы
+    terminal_init();    // Инициализация терминала (после FS)
+    
     // Главный цикл
- while (1) {
+    while (1) {
         char key = keyboard_getch();
         if (key) {
             terminal_handle_input(key);
-  }
-
+        }
+        
         terminal_update();
-for (volatile int i = 0; i < 5000; i++);
+        
+        // Простая задержка
+        for (volatile int i = 0; i < 5000; i++);
     }
 }
